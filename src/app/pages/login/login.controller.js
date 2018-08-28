f58b0c9bada2a6d5687140467793d4e9cd618344
April Wan <april.wan@pwc.com>
2018-08-28 15:10:10 +0800

    console info

--- src/app/pages/login/login.controller.js
+++ src/app/pages/login/login.controller.js
@@ -35,6 +35,7 @@ export class LoginController {
         this.$cookies.remove('loggedInUser');
       }
       this.apiService.get('src/app/shared/data/test-user.json').then(resp => {
+        console.log(resp);
         this.sessionService.setCurrentUser(resp.data);
         resp.data.status === 'NEW_USER' ? this.$state.go('update-password') : this.$state.go('home');
       }).catch(err => {
